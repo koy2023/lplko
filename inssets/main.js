3fa0e9ca3a0ca984bd5c5dd7c60bf64aad80ae20
koy2023 <111230339+koy2023@users.noreply.github.com>
2024-07-29 20:58:16 +0700

    Add files via upload

---
+++ inssets/main.js
@@ -0,0 +1,17 @@
+
+
+function filterFunction() {
+    var input, filter, div, a, i;
+    input = document.getElementById("searchBox");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("dropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
